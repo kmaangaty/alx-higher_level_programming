bd5bc2420db1d36bc3effe67d060df8e8eda078d
kmaangaty <kmaangaty@gmail.com>
2023-05-08 10:00:35 -0700

    85232000

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = f"{word[:3]}"
+word_last_2 = f"{word[7:]}"
+middle_word = f"{word[1:-1]}"
+print(f"{word_first_3}\n{word_last_2}\n{middle_word}")
