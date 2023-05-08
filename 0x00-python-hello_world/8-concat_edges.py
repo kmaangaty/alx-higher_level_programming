cd464af9dfd42a8faa59e2d48ad203376d178579
kmaangaty <kmaangaty@gmail.com>
2023-05-08 11:13:50 -0700

    85232113

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-to_print = f"{str[39:54]} {str[55:66]} {str[107:111]} {str[0:6]}"
-print(to_print)
+str = f"{str[39:54]} {str[55:66]} {str[107:111]} {str[0:6]}"
+print(str)
