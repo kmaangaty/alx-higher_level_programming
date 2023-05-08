82b9408875703017b50f9bf15d6eef31cf5feebb
kmaangaty <kmaangaty@gmail.com>
2023-05-08 10:09:41 -0700

    85232009

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = f"{word[:3]}"
-word_last_2 = f"{word[7:]}"
-middle_word = f"{word[1:-1]}"
-to_print = f"{word_first_3}\n{word_last_2}\n{middle_word}"
+word_first_3 = f"First 3 letters: {word[:3]}"
+word_last_2 = f"Last 2 letters: {word[7:]}"
+middle_word = f"Middle word: {word[1:-1]}"
+print_line = "\n"
+to_print = f"{word_first_3}{print_line}{word_last_2}{print_line}{middle_word}"
 print(to_print)
