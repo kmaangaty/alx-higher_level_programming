2074119be3b9163a1f4b0dbc28fa6a7cb8d774cb
kmaangaty <kmaangaty@gmail.com>
2023-05-09 10:04:07 -0700

    95232004

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,11 @@ type_one = "and is less than 6 and not 0"
 type_tow = "and is greater than 5"
 if number != 0 and last_digit != 0:
     if number < 0:
-        print(f"Last digit of {number} is -{last_digit} {type_one}")
+        print(f"Last digit of {number:d} is -{last_digit:d} {type_one}")
     else:
         if last_digit < 6:
-            print(f"Last digit of {number} is {last_digit} {type_one}")
+            print(f"Last digit of {number:d} is {last_digit:d} {type_one}")
         else:
-            print(f"Last digit of {number} is {last_digit} {type_tow}")
+            print(f"Last digit of {number:d} is {last_digit:d} {type_tow}")
 else:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number:d} is {last_digit:d} and is {last_digit}")
