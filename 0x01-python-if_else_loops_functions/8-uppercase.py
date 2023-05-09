94df3be354d22bcceb5db6407c60a86ff0d83567
kmaangaty <kmaangaty@gmail.com>
2023-05-09 12:55:45 -0700

    95232255

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     str_main = str
     for i in range(len(str)):
         str_main = str_main[:i] + chr(ord(str_main[i]) - 32) + str_main[i + 1:]
-    return str_main
+    print("{}".format(str_main))
