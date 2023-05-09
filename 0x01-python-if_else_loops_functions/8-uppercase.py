a893ae9dc8b8cbc0a5787c2e58a6e51c2c1ecc1d
kmaangaty <kmaangaty@gmail.com>
2023-05-09 12:53:18 -0700

    95232252

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_main = str
+    for i in range(len(str)):
+        str_main = str_main[:i] + chr(ord(str_main[i]) - 32) + str_main[i + 1:]
+    return str_main
