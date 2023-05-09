1d6b6ac0e081263f85fb4e72efce571308b6911e
kmaangaty <kmaangaty@gmail.com>
2023-05-09 11:08:58 -0700

    95232108

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-stri = ""
-for start in range(97, 97+26):
-    to_print = "{:c}".format(start, end='')
+stri = ''
+for start in range(97, 123):
+    to_print = "{:c}".format(start)
     stri = stri + to_print
-print(stri)
+print(stri, end='')
+
