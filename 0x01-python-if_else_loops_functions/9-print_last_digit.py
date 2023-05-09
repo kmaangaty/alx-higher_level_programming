a86e88ab3b3c2495e2d4bd4c82a89183be42f37a
kmaangaty <kmaangaty@gmail.com>
2023-05-09 13:26:21 -0700

    95232326

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return print("{:d}".format(int(repr(number)[-1])))
+    if number < 0:
+        number = number * -1
+    i = number % 10
+    print("{}".format(i), end='')
+    return i
