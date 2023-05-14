ea0a0349d5eaa4c33a7bfddad7605ab13b75e0aa
kmaangaty <kmaangaty@gmail.com>
2023-05-14 11:35:21 -0700

    145232135

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     sl = len(sentence)
     if sl >= 1:
-    	res = (len(sentence), sentence[:1])
-    	return res
+        res = (len(sentence), sentence[:1])
+        return res
     else:
         return (0, None)
