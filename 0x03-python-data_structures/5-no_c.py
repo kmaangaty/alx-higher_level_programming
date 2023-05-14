6dcf29e06cbb15179309a79b5cbe9a62c35eecde
kmaangaty <kmaangaty@gmail.com>
2023-05-14 11:06:33 -0700

    145232106

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    nst = [i for i in my_string if i != 'c' or i != 'C']
-    return("".join(nst))
+    nst = ""
+    for i in my_string:
+        if i != "c" and i != "C":
+            nst = nst + i
+    return nst
