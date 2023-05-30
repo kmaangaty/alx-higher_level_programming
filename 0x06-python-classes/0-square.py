1709b4e2835562798b4157b069992f12758d8b29
kmaangaty <kmaangaty@gmail.com>
2023-05-30 09:36:46 -0700

    305231936

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" Square class Define"""
+
+
 class Square:
+    """square represented"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" Square class Define"""
+
+
 class Square:
+    """square represented"""
     def __init__(self, size):
+        """Initialize a new class.
+        Args: size (int): parameter of type int.
+        """
         self.__size = size
