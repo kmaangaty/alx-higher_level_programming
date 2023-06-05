cddab079400e2186deb54bf98fec74bd72979a37
kmaangaty <kmaangaty@gmail.com>
2023-06-05 11:00:16 -0700

    0506232100

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this file Define aclass name [Rectangle]"""
+
+
+class Rectangle:
+    """this is representation of Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""this file Define aclass name [Rectangle]"""
+
+
+class Rectangle:
+    """this is representation of Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Init a new object.
+
+        Args:
+            height (int): parameter of type int.
+            width (int): parameter of type int.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter or setter for field width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter or setter for field height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the calculated value for field area in object."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the calculated value for field perimeter in object."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
