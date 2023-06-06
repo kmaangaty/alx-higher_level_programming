44796a87c8b547248cc9e90ab220971cbca0c0ec
kmaangaty <kmaangaty@gmail.com>
2023-06-06 06:25:23 -0700

    0606231625

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This file Define class [LockedClass]"""
+
+
+class LockedClass:
+    """
+    this is representation for LockedClass class
+    """
+
+    __slots__ = ["first_name"]
